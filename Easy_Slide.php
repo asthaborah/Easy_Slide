4e27fd9cf640435e5821469b93faa3aa2c90360b
asthaborah <asthaborah086@gmail.com>
2024-03-29 11:00:56 +0530

    Header

---
+++ Easy_Slide.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Plugin Name:       Easy Slider
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Slideshow plugin
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Astha Borah
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       Easy_Slider
+ * Domain Path:       /languages
+ */
