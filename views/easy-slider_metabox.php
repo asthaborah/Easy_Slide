81887599788257b9d1b2407abdecdd5d8ca59879
asthaborah <asthaborah086@gmail.com>
2024-04-01 14:34:06 +0530

    nonce created

--- views/easy-slider_metabox.php
+++ views/easy-slider_metabox.php
@@ -1,24 +1,19 @@
 <!-- Metabox html -->
-<?php 
-    $meta = get_post_meta( $post->ID );
-    $link_text = get_post_meta( $post->ID, 'easy_slider_link_text', true );
-    $link_url = get_post_meta( $post->ID, 'easy_slider_link_url', true );
-    //var_dump( $link_text, $link_url );
+<?php
+$meta = get_post_meta($post->ID);
+$link_text = get_post_meta($post->ID, 'easy_slider_link_text', true);
+$link_url = get_post_meta($post->ID, 'easy_slider_link_url', true);
 ?>
-<table class="form-table easy-slider-metabox"> 
+
+<table class="form-table easy-slider-metabox">
+    <!-- nonce created -->
+    <input type="hidden" name="easy_slider_nonce" value="<?php echo wp_create_nonce("easy_slider_nonce"); ?>">
     <tr>
         <th>
             <label for="easy_slider_link_text">Link Text</label>
         </th>
         <td>
-            <input 
-                type="text" 
-                name="easy_slider_link_text" 
-                id="easy_slider_link_text" 
-                class="regular-text link-text"
-                value="<?php echo ( isset( $link_text ) ) ? esc_html( $link_text ) : ''; ?>"
-                required
-            >
+            <input type="text" name="easy_slider_link_text" id="easy_slider_link_text" class="regular-text link-text" value="<?php echo (isset($link_text)) ? esc_html($link_text) : ''; ?>" required>
         </td>
     </tr>
     <tr>
@@ -26,14 +21,7 @@
             <label for="easy_slider_link_url">Link URL</label>
         </th>
         <td>
-            <input 
-                type="url" 
-                name="easy_slider_link_url" 
-                id="easy_slider_link_url" 
-                class="regular-text link-url"
-                value="<?php echo ( isset( $link_url ) ) ? esc_url( $link_url ) : ''; ?>"
-                required
-            >
+            <input type="url" name="easy_slider_link_url" id="easy_slider_link_url" class="regular-text link-url" value="<?php echo (isset($link_url)) ? esc_url($link_url) : ''; ?>" required>
         </td>
-    </tr>               
+    </tr>
 </table>
