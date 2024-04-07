8a143c763496e614c565cd74a141422608a3eb46
asthaborah <asthaborah086@gmail.com>
2024-04-07 23:02:43 +0530

    more sections and fields added

--- class.easy-slider-settings.php
+++ class.easy-slider-settings.php
@@ -5,6 +5,7 @@ if( ! class_exists( 'Easy_Slider_Settings' )){
     class Easy_Slider_Settings{
 
         public static $options;
+        
 
         public function __construct(){
             self::$options = get_option( 'easy_slider_options' );
@@ -21,9 +22,18 @@ if( ! class_exists( 'Easy_Slider_Settings' )){
             // section 1
             add_settings_section(
                 'easy_slider_main_section', //id
-                'How does it work?', //title
+                'How does it work?',  //title
                 null,                 //callback function
                 'easy_slider_page1'   //page 
+                
+            );
+
+            //section 2
+            add_settings_section(
+                'easy_slider_second_section',
+                'Other Plugin Options',
+                null,
+                'easy_slider_page2'
             );
 
             //field 1.1
@@ -34,6 +44,33 @@ if( ! class_exists( 'Easy_Slider_Settings' )){
                 'easy_slider_page1',
                 'easy_slider_main_section'
             );
+
+            //field 2.1
+            add_settings_field(
+                'easy_slider_title',
+                'Slider Title',
+                array( $this, 'easy_slider_title_callback' ),
+                'easy_slider_page2',
+                'easy_slider_second_section'
+            );
+
+            //field 2.2
+            add_settings_field(
+                'easy_slider_bullets',
+                'Display Bullets',
+                array( $this, 'easy_slider_bullets_callback' ),
+                'easy_slider_page2',
+                'easy_slider_second_section'
+            );
+
+            //field 2.3
+            add_settings_field(
+                'easy_slider_style',
+                'Slider Style',
+                array( $this, 'easy_slider_style_callback' ),
+                'easy_slider_page2',
+                'easy_slider_second_section'
+            );
         }
 
         //callback function for field 1.1
@@ -42,6 +79,22 @@ if( ! class_exists( 'Easy_Slider_Settings' )){
             <span>Use the shortcode [easy_slider] to display the slider in any page/post/widget</span>
             <?php
         }
+
+        //callback function for field 2.1
+        public function easy_slider_title_callback(){
+            
+        }
+        
+        //callback function for field 2.2
+        public function easy_slider_bullets_callback(){
+            
+        }
+
+        //callback function for field 2.3
+        public function easy_slider_style_callback(){
+            
+        }
+
 
     }
 }
