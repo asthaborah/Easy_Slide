430190f99d885daac6e07fa3d542c3276c089123
asthaborah <asthaborah086@gmail.com>
2024-04-11 15:49:49 +0530

    Splitting sections into tabs

--- views/settings-page.php
+++ views/settings-page.php
@@ -1,21 +1,36 @@
+<!-- setting page html -->
+
+
+<!-- creating form and this form will submit the information of our setting page to the database -->
+
+
 <div class="wrap">
-    
+
     <!-- Title -->
+
     <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
+
+    <!-- tabs -->
+    <?php
+    $active_tab = isset($_GET['tab']) ? $_GET['tab'] : 'main_options';
+    ?>
+
+    <h2 class="nav-tab-wrapper">
+        <a href="?page=easy_slider_admin&tab=main_options" class="nav-tab <?php echo $active_tab == 'main_options' ? 'nav-tab-active' : ''; ?>">Main Options</a>
+        <a href="?page=easy_slider_admin&tab=additional_options" class="nav-tab <?php echo $active_tab == 'additional_options' ? 'nav-tab-active' : ''; ?>">Additional Options</a>
+    </h2>
+
+    <!-- form -->
     <form action="options.php" method="post">
         <?php
-
-            //security : nonce
+        if ($active_tab == 'main_options') {
             settings_fields('easy_slider_group');
-
-            //display sections
             do_settings_sections('easy_slider_page1');
-
-            do_settings_sections( 'easy_slider_page2' );
-
-            //submit button
-            submit_button('Save Settings');
+        } else {
+            settings_fields('easy_slider_group');
+            do_settings_sections('easy_slider_page2');
+        }
+        submit_button('Save Settings');
         ?>
-
     </form>
 </div>
