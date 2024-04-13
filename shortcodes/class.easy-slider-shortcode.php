3cd00e9191180586a0c710b04e92c15b14ca9e46
asthaborah <asthaborah086@gmail.com>
2024-04-13 16:08:34 +0530

    registered the shortcode

---
+++ shortcodes/class.easy-slider-shortcode.php
@@ -0,0 +1,34 @@
+<?php 
+if( ! class_exists('Easy_Slider_Shortcode')){
+    class Easy_Slider_Shortcode{
+        public function __construct(){
+
+            //registered the shortcode
+            add_shortcode( 'easy_slider', array( $this, 'add_shortcode' ) );
+        }
+
+        public function add_shortcode( $atts = array(), $content = null, $tag = '' ){
+
+            $atts = array_change_key_case( (array) $atts, CASE_LOWER );
+
+            extract( shortcode_atts(
+
+                //default arguments
+                array(
+                    'id' => '',
+                    'orderby' => 'date'
+                ),
+
+                //user argument
+                $atts,
+                $tag
+            ));
+
+            //user should pass the slide id seperated by comma
+            //Absint function makes your id passed by user is integer
+            if( !empty( $id ) ){
+                $id = array_map( 'absint', explode( ',', $id ) );
+            }
+        }
+    }
+}
